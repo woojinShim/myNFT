3438f25529994ba9a2ce837169b22d868ddfdbd3
Saul Lee <saul@atomrigs.io>
2022-06-17 10:32:27 +0900

    fix: fix import error, variable errors

--- test/JuniorNFT.test.js
+++ test/JuniorNFT.test.js
@@ -1,4 +1,5 @@
-const chai,{ expect } = require("chai");
+const chai = require("chai");
+const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { solidity } = require("ethereum-waffle");
 
@@ -8,43 +9,46 @@ describe("JuniorNFT", function () {
   let JuniorNFT, contract, deployer, addr1, addr2;
 
   beforeEach(async () => {
+    // DESTRUCTURING:
     [deployer, addr1, addr2] = await ethers.getSigners();
     JuniorNFT = await ethers.getContractFactory("JuniorNFT");
     // contract = await JuniorNFT.deploy("http://baseURI", "http://defaultURI");
     contract = await JuniorNFT.deploy();
     // await contract.deployed();
   });
-  it('should set the right owner', async () => {
-    expect(await contract.owner()).to.equal(deployer.address)
-  })
+  it("should set the right owner", async () => {
+    expect(await contract.owner()).to.equal(deployer.address);
+  });
 
-  it('should emit TransferEvent if transfer succeeded', async () => {
-    expect(await contract.connect(deployer).transfer(addr1.address, 100)).to.emit(token, "TransferEvent")
-    .withArgs(deployer.address, addr1.address, 100)
-  })
+  it("should emit TransferEvent if transfer succeeded", async () => {
+    expect(await contract.connect(deployer).transfer(addr1.address, 100))
+      .to.emit(contract, "TransferEvent")
+      .withArgs(deployer.address, addr1.address, 100);
+  });
 
-  it('should revert if sender does not have enough balance', async() => {
-    await expect(token.connect(addr2).transfer(addr1.address, 100)).to.be.revertedWith("Not enough balance")
-  })
+  it("should revert if sender does not have enough balance", async () => {
+    await expect(
+      contract.connect(addr2).transfer(addr1.address, 100)
+    ).to.be.revertedWith("Not enough balance");
+  });
 
-  it('should send/receive right amount', async () => {
-    await token.connect(deployer).transfer(addr1.address, 10000)
+  it("should send/receive right amount", async () => {
+    await contract.connect(deployer).transfer(addr1.address, 10000);
 
-    expect(await token.balanceOf(addr1.address)).to.eq(10000)
-    expect(await token.balanceOf(deployer.address)).to.eq(90000)
-  })
+    expect(await contract.balanceOf(addr1.address)).to.eq(10000);
+    expect(await contract.balanceOf(deployer.address)).to.eq(90000);
+  });
 
-  
   describe("function test", () => {
     it("should function act", async () => {
-      await contract.addAdmin(addr[1].address);
+      await contract.addAdmin(addr1.address);
       console.log(await contract.Admin());
-      console.log(addr[1].address);
+      console.log(addr1.address);
     });
 
     it("should mint", async () => {
-      await contract.addAdmin(addr[1].address);
-      await contract.connect(addr[1]).adminMint(3);
+      await contract.addAdmin(addr1.address);
+      await contract.connect(addr1).adminMint(3);
       console.log(await contract.tokenURI(2));
       console.log(await contract.setBaseURI("asdf"));
       console.log(await contract.withdraw());
